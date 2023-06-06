0392f4fcdff6658cebbdbb8f2da530725054c284
KaungMyatHun-15691 <kaungmyathun7@gmail.com>
2023-06-06 23:00:58 +0630

    adding: C project

---
+++ C_Lesson1.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+int main(){
+    printf("Hello world");
+    int age ;
+    printf("\n Enter Your Age");
+    scanf("%d", &age);
+    if(age >= 20 ){
+        printf("\nI love My World");
+    }else{
+    printf("To young to love");
+    }
+}
