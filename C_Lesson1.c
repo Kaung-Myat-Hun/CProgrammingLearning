cbc569eb491051f7a9db71406ee81d4a57480698
kaung <kaungmyathun7@gmail.com>
2023-06-08 15:12:26 +0630

    learning : random C

--- C_Lesson1.c
+++ C_Lesson1.c
@@ -1,14 +1,34 @@
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<time.h>
 
 int main(){
-    printf("Hello world");
-    int age ;
-    printf("\n Enter Your Age");
-    scanf("%d", &age);
-    if(age >= 20 ){
-        printf("\nI love My World");
-    }else{
-    printf("To young to love");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Hello World from for loop\n");
     }
+    
+    int j = 0;
+    do{
+        printf("Hello from Do While loop\n");
+        j++;
+    }
+    while(j!= 3);
+
+    // int age;
+    int randNum;
+    srand(time(NULL));
+    randNum = rand()%20;
+    printf("%d\n", randNum);
+    // printf("Enter your age : ");
+    // scanf("%d", &age);
+    // if(age >= 18){
+    //     printf("You can enter casino and play games");
+    //     getch();
+    // }else{
+    //     printf("Sorry you cannot access to enter");
+    //     getch();
+    // }
 }
